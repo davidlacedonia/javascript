5d19c095c731632677816f85a0c28dda2356e913
David Lacedonia <davidlacedonia@gmail.com>
2020-06-30 08:36:13 +0200

    hoisting

---
+++ hoisting/README.md
@@ -0,0 +1,19 @@
+## Hoisting
+
+Variable and function declarations are moved from where they appear in the flow of the code to the top of the code.
+Only the declarations themeselves are hoisted, while any assignments or other executable logic are left in place.
+
+### Compiler
+
+All declarations are processed first, before any part of your code is executed.
+
+```js
+  var a = 2;
+```
+This is actually interpreted as two statements.
+_var a_ and _a = 2_
+The first statement, the declaration, is processed during the compilation phase. The second statement, the assignment, is left in place for the execution phase.
+
+Hoisting is per scope.
+
+Function declarations are hoisted, but function expressions are not.
---
+++ hoisting/hoisting.js
@@ -0,0 +1,57 @@
+{
+  a = 2;
+  var a; // hoisted to the top
+  console.log(a);
+
+  /**
+   * Translates to
+   *
+   * var a;
+   * a = 2;
+   * console.log(a) // 2
+   */
+}
+
+{
+  console.log(b);
+  var b = 2; // hoists declaration to the top, but the assignment happens later
+
+  /**
+   * Translates to
+   *
+   * var b;
+   * console.log(b) // undefined
+   * b = 2;
+   */
+}
+
+{
+  foo();
+
+  // var foo;
+  // this is a syntax error because the function declaration is hoisted first
+  // so in strict mode duplicate declarations throw the error
+  // SyntaxError: Identifier 'foo' has already been declared
+
+  function foo() {
+    console.log(1);
+  }
+
+  foo = function () {
+    console.log(2);
+  };
+}
+
+{
+  foo();
+
+  function foo() {
+    console.log('a');
+  }
+
+  function foo() {
+    console.log('b');
+  }
+
+  // subsequent function declarations override previous ones.
+}
